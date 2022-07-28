ddb4d28b7fac1b7c37def58833ef916fc8b57d48
Klaus Cedillo <klausmcedillo@gmail.com>
2022-07-28 17:07:04 -0500

    Fix

--- server.js
+++ server.js
@@ -10,9 +10,6 @@ app.use(express.static('public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-})
 
 app.get('/notes', (req, res) =>{
     res.sendFile(path.join(__dirname, '/public/notes.html'))
@@ -61,7 +58,9 @@ app.delete('/api/notes/', (req, res) =>{
     })
 })
 
-
+app.get('*', (req, res) =>{
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
 
 app.listen(PORT, () =>{
     console.log(`Listening to http://localhost:${PORT}`);
